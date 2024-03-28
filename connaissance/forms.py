95a9db05dc1e2208d0a06b36f248abbba83abb6a
Your Name <victorjacquet.13@gmail.com>
2024-03-28 11:06:47 +0100

    message d'erreur cours

--- connaissance/forms.py
+++ connaissance/forms.py
@@ -10,16 +10,24 @@ class CoursForm(forms.ModelForm):
         fields = "__all__"
         labels = {
             "titre": ("Titre de votre cours"),
+            "parent": ("Titre de votre cours à la base "),
+            "document": ("Titre de votre document"),
+            "racine": ("cochez si c'est une matière")
         }
         help_texts = {
             "titre": ("Choisissez un titre SIMPLE et conscis."),
+            "parent": ("un parent doit etre la base d'un cours "),
+            "document": ("Choisissez un titre SIMPLE et conscis."),
+            "racine": ("une matière est la base des parents ou des cours si il n'y a pas de parents")
         }
         error_messages = {
             "titre": {
-                "max_length": ("Choisissez un titre de moins de 20 lettres."),
-            },
+                "max_length": ("Choisissez un titre de moins de 20 caractère."),
+            }
+            "document"{
+                "max_lenght": ("Choisissez un titre de moins de 100 caractères.")
+            }
         }
-
     disable_client_side_validation = True
 
     def __init__(self, *args, **kwargs):
